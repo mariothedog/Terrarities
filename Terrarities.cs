1af4f8c98b08ef00aa56b25d67ce77421219130c
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-09-21 18:30:36 +0100

    Name changed to "Terrarities"

---
+++ Terrarities.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace Terrarities
+{
+	public class Terrarities : Mod
+	{
+	}
+}
--- Terrarity.cs
+++
@@ -1,8 +0,0 @@
-using Terraria.ModLoader;
-
-namespace Terrarity
-{
-	public class Terrarity : Mod
-	{
-	}
-}
