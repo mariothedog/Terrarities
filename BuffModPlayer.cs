9ae2444355a83e119e7534333b749e6574bb4b15
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-09-21 18:23:14 +0100

    Band of Honey Regeneration

---
+++ BuffModPlayer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Terrarity
+{
+	public class BuffModPlayer : ModPlayer
+	{
+		public bool HasBandOfHoneyRegen;
+
+		public override void ResetEffects()
+		{
+			HasBandOfHoneyRegen = false;
+		}
+
+		public override void OnHitByNPC(NPC npc, int damage, bool crit)
+		{
+			if (HasBandOfHoneyRegen)
+			{
+				for (int i = 0; i < Main.rand.Next(2, 4); i++)
+				{
+					Vector2 beeVelocity = new Vector2(
+						Main.rand.Next(-35, 36) * 0.02f,
+						Main.rand.Next(-35, 36) * 0.02f
+						);
+					int beeDamage = Main.rand.Next(28, 32);
+					Projectile.NewProjectile(player.position, beeVelocity, ProjectileID.Bee, beeDamage, 0, player.whoAmI);
+				}
+			}
+		}
+	}
+}
---
+++ Items/Accessories/BandOfHoneyRegeneration.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Terrarity.Items.Accessories
+{
+	public class BandOfHoneyRegeneration : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.width = 24;
+			item.height = 28;
+			item.value = 10000;
+			item.rare = ItemRarityID.Green;
+			item.accessory = true;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.AddBuff(BuffID.Honey, 1);
+
+			BuffModPlayer buffModPlayer = player.GetModPlayer<BuffModPlayer>();
+			buffModPlayer.HasBandOfHoneyRegen = true;
+		}
+	}
+}
